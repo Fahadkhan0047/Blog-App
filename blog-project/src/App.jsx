f90f253a5ab76592598637fd8988519c287d40aa
Fahadkhan0047 <you@example.com>
2024-04-26 06:59:59 +0530

    configured Redux-Toolkit

--- blog-project/src/App.jsx
+++ blog-project/src/App.jsx
@@ -1,21 +1,42 @@
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import {useDispatch} from 'react-redux'
 import { useState , useEffect} from 'react'
 import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  const [loading, setloading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
 
   useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout({}))
+      }
+    })
+    .finally(()=>setLoading(false))
     
   }, [])
   
-  return (
-    <>
-     <h1>Blog App using Appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
+   
+
 }
 
 export default App
