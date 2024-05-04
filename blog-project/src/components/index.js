2f8b1b8b20eeddd05b32089f2349f8930c47cdbf
Fahadkhan0047 <kfahaduu@gmail.com>
2024-05-05 02:07:09 +0530

    ..

--- blog-project/src/components/index.js
+++ blog-project/src/components/index.js
@@ -3,11 +3,13 @@
  import Logo from "./Logo";
  import LogoutBtn from "./Header/LogoutBtn";
  import Container from './container/Container'
+ import RTE from "./RTE.JSX";
 
  export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
  }
